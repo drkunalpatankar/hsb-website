53c1361e8d487e7cd843fb94fc9c4480ec6dea2f
drkunalpatankar <drkunalpatankar@gmail.com>
2026-01-22 11:01:32 +0530

    Fix Home.jsx syntax errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Philosophy from '../components/Philosophy';
+import Treatments from '../components/Treatments';
 import SmileAssessmentSection from '../components/SmileAssessmentSection';
+import WhyChooseUs from '../components/WhyChooseUs';
+import Education from '../components/Education';
+import Transformations from '../components/Transformations';
+import DoctorIntro from '../components/DoctorIntro';
+import CTA from '../components/CTA';
 
 const Home = () => {
     return (
@@ -11,25 +20,11 @@ const Home = () => {
             <SmileAssessmentSection />
             <WhyChooseUs />
             <Education />
-            import Transformations from '../components/Transformations';
-            import DoctorIntro from '../components/DoctorIntro';
-            import CTA from '../components/CTA';
-
-const Home = () => {
-    return (
-            <>
-                <Hero />
-                <Philosophy />
-                {/* On Home, we might show a summarized version or just the full sections as teasers. 
-          For now, keeping the rich detail as it serves as a good overview. */}
-                <Treatments />
-                <WhyChooseUs />
-                <Education />
-                <Transformations />
-                <DoctorIntro />
-                <CTA />
-            </>
-            );
+            <Transformations />
+            <DoctorIntro />
+            <CTA />
+        </>
+    );
 };
 
-            export default Home;
+export default Home;
